d43bbdcd124f26f72ae05ec6e28dca755e408205
zhengyang2 <67302728+zhengyang2@users.noreply.github.com>
2025-01-25 19:39:07 +0800

    edit movement to new uml

--- core/src/main/java/io/github/team2/movement.java
+++ core/src/main/java/io/github/team2/movement.java
@@ -5,37 +5,21 @@ import com.badlogic.gdx.math.Vector2;
 
 public interface movement {
 	
-	// default coordinates methods
-	
-	public Vector2 getCoordinate();
-	public void setCoordinate(Vector2 vec);
-	
-	// 8 point movement system 
-	public void moveUp(Vector2 position, Vector2 direction);
-	public void moveleft(Vector2 position, Vector2 direction);
-	public void moveRight(Vector2 position, Vector2 direction);
-	public void moveDown(Vector2 position, Vector2 direction);
-	
-
-	public void moveUpLeft(Vector2 position, Vector2 direction);
-	public void moveUpRight(Vector2 position, Vector2 direction);
-	public void moveDownLeft(Vector2 position, Vector2 direction);
-	public void moveDownRight(Vector2 position, Vector2 direction);
-
-	// angle section 
-	public int getAngle();
-	public int setAngle(int ang);
+	// movement
+	public void moveTo(Vector2 newPos);
 	
 	
 	// rotation movement 
-	public void rotateTo(int num);
-	public void rotateLeft(int num);
-	public void rotateRight(int num);
+	public void rotateTo(float num);
 	
 	
-	// distance methods
+	public default boolean checkOutOfBound() {
+		
+		return true;
+	}
 	
-	public Vector2 getDistance(Vector2 current, Vector2 destination);
 	
+	}
+
+
 	
-}
