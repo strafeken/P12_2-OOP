4284934f634a366eaa29e2cdf691056be18fec20
Strafeken <39856282+strafeken@users.noreply.github.com>
2025-02-09 22:15:27 +0800

    Update GameMaster.java

--- core/src/main/java/io/github/team2/GameMaster.java
+++ core/src/main/java/io/github/team2/GameMaster.java
@@ -12,36 +12,40 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class GameMaster extends ApplicationAdapter {
     private SpriteBatch batch;
     private ShapeRenderer shape;
+    private AudioManager am;
     private SceneManager sm;
 
     @Override
     public void create() {
+        batch = new SpriteBatch();
+        shape = new ShapeRenderer();
+        
+        am = AudioManager.getInstance();
         sm = SceneManager.getInstance(SceneManager.class);
-
-        // Initialize audio with correct paths and volume
-        AudioManager.getInstance().loadSoundEffect("start", "sounds/start.mp3");
-        AudioManager.getInstance().loadSoundEffect("ding", "sounds/ding.mp3");
-        AudioManager.getInstance().loadSoundEffect("mainmenu", "sounds/mainmenu.mp3");
-        AudioManager.getInstance().setVolume(1.0f);
+        
+        // initialize audio with correct paths and volume
+        am.loadSoundEffect("start", "sounds/start.mp3");
+        am.loadSoundEffect("ding", "sounds/ding.mp3");
+        am.loadSoundEffect("mainmenu", "sounds/mainmenu.mp3");
+        am.setVolume(1.0f);
 
         sm.addScene(SceneID.MAIN_MENU, new MainMenu());
         sm.addScene(SceneID.GAME_SCENE, new GameScene());
         sm.addScene(SceneID.PAUSE_MENU, new PauseMenu());
         sm.addScene(SceneID.GAME_OVER, new GameOverScreen());
-
+        
         sm.setNextScene(SceneID.MAIN_MENU);
-
-        batch = new SpriteBatch();
-        shape = new ShapeRenderer();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0, 0, 0.2f, 1);
+        
         sm.update();
         batch.begin();
         sm.draw(batch);
         batch.end();
+        
         shape.begin(ShapeRenderer.ShapeType.Filled);
         sm.draw(shape);
         shape.end();
@@ -51,6 +55,6 @@ public class GameMaster extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         shape.dispose();
-        AudioManager.getInstance().dispose();
+        am.dispose();
     }
 }
