55cb65895eea564685a1ba31a71f581da76286a4
Strafeken <39856282+strafeken@users.noreply.github.com>
2025-02-14 15:35:49 +0800

    Update CollisionListener.java

--- core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
+++ core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
@@ -1,10 +1,6 @@
 package io.github.team2.CollisionSystem;
 
 import io.github.team2.EntitySystem.Entity;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.Manifold;
-
 
 public interface CollisionListener {
     void onCollision(Entity entityA, Entity entityB, CollisionType type);
