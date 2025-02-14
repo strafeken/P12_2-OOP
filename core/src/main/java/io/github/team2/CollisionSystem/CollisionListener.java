8500d50990d2c7dff17c84895ee263eef682137b
Strafeken <39856282+strafeken@users.noreply.github.com>
2025-02-14 15:34:36 +0800

    revert CollisionListener

--- core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
+++ core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
@@ -7,9 +7,5 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 
 
 public interface CollisionListener {
-    default void beginContact(Contact contact) {}
-    default void endContact(Contact contact) {}
-    default void preSolve(Contact contact, Manifold oldManifold) {}
-    default void postSolve(Contact contact, ContactImpulse impulse) {}
     void onCollision(Entity entityA, Entity entityB, CollisionType type);
 }
