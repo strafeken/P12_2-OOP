6db65bf4e64a3806851225a966a1ed1b1fcfbd80
Strafeken <39856282+strafeken@users.noreply.github.com>
2025-02-15 16:45:01 +0800

    created DisplayManager utility class

--- core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
+++ core/src/main/java/io/github/team2/CollisionSystem/CollisionListener.java
@@ -3,5 +3,5 @@ package io.github.team2.CollisionSystem;
 import io.github.team2.EntitySystem.Entity;
 
 public interface CollisionListener {
-    void onCollision(Entity entityA, Entity entityB, CollisionType type);
+    void onCollision(Entity a, Entity b, CollisionType type);
 }
