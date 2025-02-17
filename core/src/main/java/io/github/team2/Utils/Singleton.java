3095aa7b49a6323947705e810150ce638306107f
Strafeken <39856282+strafeken@users.noreply.github.com>
2025-02-17 15:53:19 +0800

    Update Singleton.java

--- core/src/main/java/io/github/team2/Utils/Singleton.java
+++ core/src/main/java/io/github/team2/Utils/Singleton.java
@@ -1,27 +1,32 @@
 package io.github.team2.Utils;
 
-public abstract class Singleton<T> {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-    private static Singleton<?> instance;
+public abstract class Singleton<T> {
+    private static final Map<Class<?>, Singleton<?>> instances = new ConcurrentHashMap<>();
     
     protected Singleton() {
-    	
+    	// prevent external instantiation
     }
-    
+
     // uses double-checked locking principle
     @SuppressWarnings("unchecked")
-	public static <T extends Singleton<T>> T getInstance(Class<T> singleton) {
-        if (instance == null) {
-            synchronized (Singleton.class) {
-                if (instance == null) {
+    public static <T extends Singleton<T>> T getInstance(Class<T> singleton) {
+        if (!instances.containsKey(singleton)) {
+            synchronized (singleton) {
+                // double-check if the instance is still not created
+                if (!instances.containsKey(singleton)) {
                     try {
-                        instance = singleton.getDeclaredConstructor().newInstance();
+                        // create the instance and store it in the map
+                        instances.put(singleton, singleton.getDeclaredConstructor().newInstance());
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
         }
-        return (T) instance;
+        
+        return (T) instances.get(singleton);
     }
 }
